3d168f2c2376ea1e75d94b20b8c5b16f07ad116c
Rohini1509 <64795200+Rohini1509@users.noreply.github.com>
2022-03-31 18:40:21 +0530

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,32 @@
+# Python program to print
+# mean of elements
+
+# list of elements to calculate mean
+import csv
+with open('height-weight.csv', newline='') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+print(file_data)
+file_data.pop(0)
+
+# print(file_data)
+# sorting data  to get the height of people.
+new_data=[]
+
+print(len(file_data))
+for i in range(len(file_data)):
+	n_num = file_data[i][1]
+	new_data.append(float(n_num))  
+
+# #getting the mean
+n = len(new_data)
+
+print(new_data)
+total =0
+for x in new_data:
+    total += x
+
+mean = total / n
+
+print("Mean / Average is: " + str(mean))
+
